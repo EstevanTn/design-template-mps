e6626adbb59c349ae81b557704f1ac97d3cc701f
Alexander Estevan Tume Naquiche <tunaqui@outlook.es>
2018-09-25 11:57:49 -0500

    slimScroll plugin, edición de página area

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,40 @@
-import $ from 'jquery';
+let $ = require('jquery');
+let jQuery = require('jquery');
+global.$ = $;
+global.jQuery = jQuery;
 
-global.$ = global.jQuery = $;
+require('popper.js');
 
-import 'popper.js';
+require('bootstrap');
 
-import 'bootstrap';
+require('jquery-slimscroll');
 
 //import * as Vue from 'vue';
 
 //import '../vue/loading.vue';
 
-let headinglass = false;
+$('#main').slimScroll({
+    height: '100%',
+    size: '8px',
+    alwaysVisible: true,
+    allowPageScroll: false,
+    disableFadeOut: false,
+    railVisible: true,
+    railOpacity: 0.15,
+});
 
-/*$('.wrapper__main').scroll(function() {
-    var show = $('.wrapper__main').scrollTop() >= 200;
-    if(show) {
-        if(!headinglass) {
-            $('.wrapper').addClass('no-heading-picture');
-            headinglass = true;
-        }
-    } else {
-        if(headinglass) {
-            $('.wrapper').removeClass('no-heading-picture');
-            headinglass = false;
-        }
+$('#news-primary .news__panel-body').slimScroll({
+    height: '155px',
+    width: '100%',
+});
+
+//Sidebar
+$('li a', '#sidebar-area').on('click', function(e) {
+    var li = $(this).closest('li');
+    var submenu = li.find('ul');
+    if(submenu.length > 0) {
+        submenu.toggle('swing', function(e){
+            $(li).toggleClass('active');
+        });
     }
-});*/
+});
